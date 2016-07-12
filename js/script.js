3a1a56cd8e94ff68be8f97bda49ddba7ce59fd37
ALex Khlopkov <ALex Khlopkov>
2016-07-12 17:42:56 +0300

    update 1 qa

--- js/script.js
+++ js/script.js
@@ -179,4 +179,15 @@ $(document).ready(function () {
       setTimeout(nextState, delay);
     }
     nextState();
+
+    //Scroll to
+    $('a[href^="#"]').on('click', function(event) {
+      var target = $(this.getAttribute('href'));
+      if( target.length ) {
+          event.preventDefault();
+          $('html, body').stop().animate({
+              scrollTop: target.offset().top
+          }, 1000);
+      }
+    });
 });
