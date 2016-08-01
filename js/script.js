b5eb4270d43132cb7e2697d47d0bd7d4e5c50bb4
nivaschenko <ivaschenko.n@gmail.com>
2016-08-01 11:27:47 +0300

    fix bug with menu

--- js/script.js
+++ js/script.js
@@ -251,7 +251,6 @@ $.fn.extend({
     $('a[href^="#"]').on('click', function(event) {
       var target = $(this.getAttribute('href'));
       if( target.length ) {
-          event.preventDefault();
           $('html, body').stop().animate({
               scrollTop: target.offset().top
           }, 1000);
