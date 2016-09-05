02e64978ddcbbd6557ab7db045cf092b624b1e0b
DELL\ALex <flopick@gmail.com>
2016-09-05 15:00:46 +0300

    update logo and googlemaps

--- js/main.js
+++ js/main.js
@@ -278,7 +278,7 @@ $(document).ready(function () {
   });
   //Contact US block - googlemap with marker and settings for this
   // The latitude and longitude of your business / place
-	var position = [61.456410, 5.840677];
+	var position = [41.0621212, -73.5423603];
 	function showGoogleMaps() {
 	    var latLng = new google.maps.LatLng(position[0], position[1]);
 	    var mapOptions = {
@@ -288,7 +288,7 @@ $(document).ready(function () {
           scrollwheel: false,
           navigationControl: false,
           mapTypeControl: false,
-          draggable: false,
+          // draggable: false,
 	        mapTypeId: google.maps.MapTypeId.ROADMAP,
 	        center: latLng,
           styles: [{"featureType":"all","elementType":"labels.text.fill","stylers":[{"saturation":36},{"color":"#000000"},{"lightness":40}]},{"featureType":"all","elementType":"labels.text.stroke","stylers":[{"visibility":"on"},{"color":"#000000"},{"lightness":16}]},{"featureType":"all","elementType":"labels.icon","stylers":[{"visibility":"off"}]},{"featureType":"administrative","elementType":"geometry.fill","stylers":[{"color":"#000000"},{"lightness":20}]},{"featureType":"administrative","elementType":"geometry.stroke","stylers":[{"color":"#000000"},{"lightness":17},{"weight":1.2}]},{"featureType":"landscape","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":20}]},{"featureType":"poi","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":21}]},{"featureType":"road.highway","elementType":"geometry.fill","stylers":[{"color":"#000000"},{"lightness":17}]},{"featureType":"road.highway","elementType":"geometry.stroke","stylers":[{"color":"#000000"},{"lightness":29},{"weight":0.2}]},{"featureType":"road.arterial","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":18}]},{"featureType":"road.local","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":16}]},{"featureType":"transit","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":19}]},{"featureType":"water","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":17}]}]
@@ -298,7 +298,7 @@ $(document).ready(function () {
 	    // Show the default red marker at the location
 	    marker = new google.maps.Marker({
 	        // position: latLng,
-					position: {lat: 61.456410, lng: 5.840677},
+					position: {lat: 41.0621212, lng: -73.5423603},
 	        map: map,
 	        draggable: false,
 	        animation: google.maps.Animation.DROP
