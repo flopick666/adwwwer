405aec6b4cf5773c1645ab50682ddea9123c22bd
nivashchenko <ivaschenko.n@gmail.com>
2016-09-06 17:41:30 +0300

    add emails

---
+++ server.php
@@ -0,0 +1,12 @@
+<?php
+
+
+foreach ( $_POST as $k => $v) {
+	$params[$k] = addslashes(strip_tags(trim($v)));
+}
+$message = 'Name: ' . $params['name'] . PHP_EOL
+			. 'Email: ' . $params['email'] . PHP_EOL
+			. 'Phone: ' . $params['phone'] . PHP_EOL
+			. 'Message: ' . $params['message'] . PHP_EOL;
+mail('ivaschenko.n@gmail.com', 'Contacts from adwwwer.1pls1.com', $message);
+print_r($message);
